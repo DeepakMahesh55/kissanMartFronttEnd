f8f9db0a872ebb1c96eb98658b1ed738b9a08aaa
Deepak <deepakmahesh55@gmail.com>
2026-03-10 23:09:47 +0530

    added landingPage

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import RegisterPage from './components/customer/RegisterPage';
 import Login from './components/Login';
 import AdminDashboard from './components/admin/AdminDashboard';
 import CustomerDashboard from './components/customer/CustomerDashboard';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/>
+          <Route path='/' element={<LandingPage/>}/>
           <Route path='/register' element={<RegisterPage/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/AdminDashboard' element={<AdminDashboard/>}/>
