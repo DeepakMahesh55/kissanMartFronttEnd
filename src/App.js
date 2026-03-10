47dab7c96a2eb06466649bd0baa57e910c3639cd
Deepak <deepakmahesh55@gmail.com>
2026-03-10 11:30:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import RegisterPage from './components/customer/RegisterPage';
+import Login from './components/Login';
+import AdminDashboard from './components/admin/AdminDashboard';
+import CustomerDashboard from './components/customer/CustomerDashboard';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/AdminDashboard' element={<AdminDashboard/>}/>
+          <Route path='/CustomerDashboard' element={<CustomerDashboard/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
